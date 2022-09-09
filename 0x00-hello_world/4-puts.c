0090fe8b527342f928bfcb7562da123eb7797827
PreciousGift05 <preciousgiftchukwuemeka@gmail.com>
2022-09-08 17:31:41 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - entry point
- *
- *   * Return: 0
- *
- *    */
+
+ * main - entry point
+
+ * Return: 0
+
+ */
 
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+	return (0);
 
-}
+]
