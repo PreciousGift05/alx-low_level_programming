eba5e9232b34ca925818b234f0cbe246bf3378fc
PreciousGift05 <preciousgiftchukwuemeka@gmail.com>
2022-09-09 20:44:48 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - Entry point
+ *  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -24,5 +24,5 @@ int main(void)
 	}
 	putchar('\n');
 
-	return (0);
+ 	return (0);
 }
