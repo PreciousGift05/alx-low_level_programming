ca3bde0ceb92ad042dadc391d5de9a1d2e737dfc
PreciousGift05 <preciousgiftchukwuemeka@gmail.com>
2022-09-19 19:11:54 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - Write a function that prints a string, followed
+ * by a new line
+ *
+ * @str: string to print
+ *
+ * Return: Nothing
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
