dfcdef38366e2e8d9105268c368593461605652a
PreciousGift05 <preciousgiftchukwuemeka@gmail.com>
2022-10-06 11:04:58 -0700

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 		}
 
 	}
-		mul = atol(argv[1]) * atol(argv[2]);
-		printf("%lu\n", mul);
-		return (0);
+	mul = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", mul);
+	return (0);
 }
