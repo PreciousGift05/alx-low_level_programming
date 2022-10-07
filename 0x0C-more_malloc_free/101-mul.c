39331a09c5e5396e4089ada8a52ef950f07c26f7
PreciousGift05 <preciousgiftchukwuemeka@gmail.com>
2022-10-06 21:29:11 -0700

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,19 +1,66 @@
-:#include "main.h"
+#include "main.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
- * _atoi_digit - convert a char to integer.
- * @x: character to convert.
- * Return: integer.
+ *  main - multiplies two positive numbers.
+ *  @argc: number of arguments
+ *
+ *  @argv: the values in each argc element
+ *  Description: its like infinite add but with multiplication
+ *  Return: print the result. return 0 if success
  */
-
-int _atoi_digit(char x)
+# IF EITHER NUMBER ARE ZERO RETURN 0;
+##SHOULD CHECK IF BEGINNING LEADING NUMBER IS ZERO.
+int main(int argc, char **argv)
 {
-		unsigned int res;
-
-			if (x <= '9' && x >= '0')
-						res = x - '0';
-	return (res);
+int size1 = 0;
+int size2 = 0;
+int product_size = 0;
+int product = 0;
+int overflow = 0;
+int index = 0;
+int tab = 0;
+while (argv[1][size1])//strlength and checks if digit
+{
+ISDIGIT\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#################
+size1++;
 }
-
-
+while (argv[2][size2])//strlength and checks if digit
+{
+ISITADIGIT########################################################
+size2++;
+}
+product_size = size1 + size2 + 1;//calculate the range we need
+product = malloc(sizeof(char) * product_size);//allocates space
+if (!product)
+return (98);
+while (product_size >= 0)//initialize all to 0
+product[product_size] = '0';
+size1--;//because length is 1 more than index
+size2--;
+while (size2 >= 0)
+{
+index = tab;
+for (j = size1; j >= 0; j--)
+product = (argv[1][j]-'0') * (argv[2][size2]-'0');
+product += product[index] -'0';
+product += overflow;
+overflow = product / 10;
+product[index] = product % 10 + '0';
+index++;
+}
+if (overflow)
+{
+product[index] = overflow;
+index++;
+}
+overflow = 0;
+size2--;
+tab++;
+}
+product[index] ='\0';
+reversestring\\\\\###########################################################
+printf("%s\n", product);
+free(product);
+return (0);
+}
