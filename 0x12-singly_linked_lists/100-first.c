467e80a79400584161a44aafdeb70d853f782bc4
PreciousGift05 <111032569+PreciousGift05@users.noreply.github.com>
2022-10-22 22:57:06 +0100

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -17,6 +17,6 @@
  */
       	void startupfun(void)
         {
-		            printf("You're beat! and yet, you must allow,");
+		        printf("You're beat! and yet, you must allow,");
 	            	printf("\nI bore my house upon my back!\n");
-	      }
+	}
